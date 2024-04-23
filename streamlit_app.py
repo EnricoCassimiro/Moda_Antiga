b4263052d368c30420042a241ed073af3d3c280e
EnricoCassimiro <155919496+EnricoCassimiro@users.noreply.github.com>
2024-04-23 18:18:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 st.header('Gráfico de linhas')
@@ -11,7 +11,6 @@ chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
 
 
 
