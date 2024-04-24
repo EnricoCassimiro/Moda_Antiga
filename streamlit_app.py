1fcae353f766df5fe4208952f7bab2ff10c8ff14
EnricoCassimiro <155919496+EnricoCassimiro@users.noreply.github.com>
2024-04-23 22:22:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,24 @@ st.write('Este site está atualmente em fase de teste. Agradecemos sua paciênci
 with st.container():
     st.write("---")
 ###################################################
-#COLOCAR UM GRAFICO AQUI
+data = pd.read_csv('Trabalho - Estoque - Blusa.csv')
+
+# Gráfico de estoque por nome de roupa
+def plot_stock_chart(data):
+    plt.figure(figsize=(15, 8))
+    plt.bar(data['Nome'], data['Estoque'], color='skyblue')
+    plt.title('Estoque de Roupas por Nome')
+    plt.xlabel('Nome da Roupa')
+    plt.ylabel('Estoque')
+    plt.xticks(rotation=90, ha='right')
+    plt.tight_layout()
+    st.pyplot()
+
+# Interface do Streamlit
+st.title('Análise de Estoque de Roupas')
+
+# Mostrar o gráfico de estoque por nome de roupa
+plot_stock_chart(data)
 ###################################################
 
 ###################################################
